faea77dd5268cd712039230ccae0e5c7fb73b545
Unknown <fratilapo@gmail.com>
2017-10-05 20:08:45 +0200

    Update Agent anf World representation
    Add behaviours and intialized sensory motor map

--- Bedau.py
+++ Bedau.py
@@ -8,13 +8,30 @@ behaviours = []
 
 
 class World():
-    def __init__(self, size):
-        self.world_size = size
-        self.world = np.zeros((size, size), dtype=int)
+    def __init__(self, world_size, pop_size, mutation_rate):
+        self.world_size = world_size
+        self.world = np.zeros((world_size, world_size), dtype=int)
         self.peak_resource = 255
         self.bin_size = self.peak_resource // 4
+        self.mutation_rate = mutation_rate
         self.meta_mutation = 0          # mutation rate of mutation: μ_μ
         self.meta_mutation_range = 0    # mutation rate varies in [μ-ε,μ+ε]: ε
+        # possible behaviours are 1-15 steps in 8 compass direction + (0,0)
+        self.behaviours = [(0, 0)]
+        for inc in range(1, 16):
+            self.behaviours.append((inc, 0))
+            self.behaviours.append((-inc, 0))
+            self.behaviours.append((0, inc))
+            self.behaviours.append((0, -inc))
+            self.behaviours.append((inc, inc))
+            self.behaviours.append((-inc, -inc))
+            self.behaviours.append((inc, -inc))
+            self.behaviours.append((-inc, inc))
+
+        self.population = []
+        for i in range(0, pop_size):
+            self.population.append(
+                Agent(self.world_size, self.behaviours, self.mutation_rate))
 
     def generate_resources(self, loc: Location):
         """
@@ -106,15 +123,19 @@ class World():
 
 class Agent():
 
-    def __init__(self, world_size):
+    def __init__(self, world_size, behaviours, mutation_rate):
         # current location: x
         self.position = (random.choice(range(0, world_size)),
                          random.choice(range(0, world_size)))
-        self.sensory_state = 0         # current sensory state: s
-        self.resources = 0              # current reservoir of resources: E
-        self.sensory_motor_map = []     # sensory motor map: φ
+        self.sensory_state = 0          # current sensory state: s
+        self.resources = 0.             # current reservoir of resources: E
         self.current_beaviour = (0, 0)  # update loc: loc' = loc + behav
-        self.mutation = 0               # mutation rate of sensor_map's loci: μ
+        self.mutation = mutation_rate   # mutation rate of sensor_map's loci: μ
+        self.sensory_motor_map = []     # sensory motor map: φ
+        self.behaviours = behaviours
+
+        for i in range(0, 1024):
+            self.sensory_motor_map.append(random.choice(behaviours))
 
     def sum(self, tuple_1, tuple_2):
         return tuple(map(operator.add, tuple_1, tuple_2))
@@ -123,14 +144,16 @@ class Agent():
 def main():
     print("Start")
     world_size = 21
-    world = World(world_size)
+    world = World(world_size, 1, 0.005)
     world.print_world()
     world.generate_resources((11, 11))
     world.print_world()
     print(world.get_sensory_state((12, 12)))
     print(world.get_sensory_state((11, 11)))
     print(world.get_sensory_state((10, 10)))
-    creature = Agent(world_size)
+    print(world.behaviours)
+    print(len(world.behaviours))
+    print(len(world.population[0].sensory_motor_map))
     print("End")
 
 
